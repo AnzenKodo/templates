a67e337536663e22f27fccb2f731e665e2e74c2f
AnzenKodo <AnzenKodo@users.noreply.github.com>
2025-06-07 17:53:01 +0530

    updated build.c

--- build.c
+++ build.c
@@ -1,8 +1,8 @@
-#include "base/base_include.h"
-#include "os/os_include.h"
+#include "src/base/base_include.h"
+#include "src/os/os_include.h"
 
-#include "base/base_include.c"
-#include "os/os_include.c"
+#include "src/base/base_include.c"
+#include "src/os/os_include.c"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,7 +10,7 @@
 
 #define PROJECT_NAME    "Scuttle"
 #define PROJECT_VERSION "0.1"
-#define BUILD_MAIN_FILE "main.c"
+#define BUILD_MAIN_FILE "src/scuttle/scuttle_entry_point.c"
 #define BUILD_DIR       "build"
 
 const char *help_message = "build.c: C file that build's C projects.\n"
@@ -19,34 +19,34 @@ const char *help_message = "build.c: C file that build's C projects.\n"
 "   build                   Build project\n"
 "   run                     Run project\n"
 "   build-run               Build and Run project\n"
-"   test                    Test project\n"
+"   build-debugger          Build for Debugger\n"
+"   test                    Test project (Requires: valgrid, typos)\n"
+"   profiler                Runs Profiler (Requires: perf)\n"
 "   version --version -v    Print project version\n"
 "   help --help -h          Print help\n";
 
 char cmd[100] = "";
 
-fn void build_cmd_append(char *cmd, char *src);
-fn void build_cmd_finish(char *cmd);
-fn void build_cmd_run(char *cmd);
-fn bool build_is_cpp_file(Str8 filename);
+internal void build_cmd_append(char *cmd, char *src);
+internal void build_cmd_finish(char *cmd);
+internal void build_cmd_run(char *cmd);
 
-fn void build_compile_cc(char *cmd) {
+internal void build_compile_cc(char *cmd) {
     build_cmd_append(cmd, "cc "BUILD_MAIN_FILE);
     build_cmd_append(cmd, " -o ./"BUILD_DIR"/"PROJECT_NAME); // Output
-    build_cmd_append(cmd, " -ggdb -g3");                     // Debug
+    build_cmd_append(cmd, " -ggdb -g3 -DBUILD_DEBUG");       // Debug
     build_cmd_append(cmd, " -Wall -Wextra");                 // Warnings
-    build_cmd_append(cmd, " -lX11 -lXrandr -lm");            // Libs
-    build_cmd_append(cmd, " -DBUILD_DEBUG -O3 -fsanitize=address");             // Define
+    // Security
+    build_cmd_append(cmd, " -mshstk -fstack-protector -fcf-protection=full");
+    // Disable useless warnings in C
+    build_cmd_append(cmd, "  -Wno-incompatible-pointer-types -Wno-override-init");
+    // Performance
+    // build_cmd_append(cmd, " -mavx2 -O3");
+    // Libs
+    build_cmd_append(cmd, " -lm -lxcb -lXau -lXdmcp -lxcb-image -lX11");
 }
 
-fn void build_compile_mingw(char *cmd) {
-    build_cmd_append(cmd, "x86_64-w64-mingw32-gcc  "BUILD_MAIN_FILE);
-    build_cmd_append(cmd, " -o ./"BUILD_DIR"/"PROJECT_NAME); // Output
-    build_cmd_append(cmd, " -ggdb -g3");                     // Debug
-    build_cmd_append(cmd, " -Wall -Wextra");                 // Warnings
-}
-
-fn void build_compile(char *cmd)
+internal void build_compile(char *cmd)
 {
     if (os_dir_make(str8(BUILD_DIR))) {
         printf("Created `"BUILD_DIR"` directory.\n");
@@ -54,38 +54,38 @@ fn void build_compile(char *cmd)
 
     printf("Compiling:\n");
     build_compile_cc(cmd);
-    build_cmd_append(cmd, " -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-unused-but-set-variable -Wno-missing-braces"); // Disable useless warnings
-    if (!build_is_cpp_file(str8(BUILD_MAIN_FILE))) {
-        build_cmd_append(cmd, "  -Wno-incompatible-pointer-types -Wno-override-init"); // Disable useless warnings in C
-    }
+    // Disable useless warnings
+    build_cmd_append(cmd, " -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-unused-but-set-variable -Wno-missing-braces");
+    // build_cmd_append(cmd, " -static");
+    build_cmd_append(cmd, " -fsanitize=address");
     build_cmd_finish(cmd);
 }
 
-fn void build_run(char *cmd)
+internal void build_run(char *cmd)
 {
     build_cmd_append(cmd, "./"BUILD_DIR"/"PROJECT_NAME);
     printf("Running:\n");
     build_cmd_finish(cmd);
 }
 
-fn void build_run_wine(char *cmd) {
-    build_cmd_append(cmd, "wine ./"BUILD_DIR"/"PROJECT_NAME);
-    printf("Running:\n");
+internal void build_debugger(char *cmd)
+{
+    if (os_dir_make(str8(BUILD_DIR))) {
+        printf("Created `"BUILD_DIR"` directory.\n");
+    }
+
+    printf("Compiling:\n");
+    build_compile_cc(cmd);
+    // Disable useless warnings
+    build_cmd_append(cmd, " -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-unused-but-set-variable -Wno-missing-braces");
     build_cmd_finish(cmd);
 }
 
-fn void build_test(char *cmd)
+internal void build_test(char *cmd)
 {
-    printf("Test Compiling in C:\n");
-    build_cmd_append(cmd, "cc -x c ");
+    printf("Compiling:\n");
     build_compile_cc(cmd);
-    build_cmd_append(cmd, " -Wno-unused-function -Wno-incompatible-pointer -Wno-incompatible-pointer-types -Wno-override-init -Wno-missing-braces"); // Disable useless warnings
-    build_cmd_finish(cmd);
-
-    printf("Test Compiling in C++:\n");
-    build_cmd_append(cmd, "cc -x c++ ");
-    build_compile_cc(cmd);
-    build_cmd_append(cmd, " -Wno-unused-function -Wno-missing-braces"); // Disable useless warnings
+    build_cmd_append(cmd, " -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-missing-braces");
     build_cmd_finish(cmd);
 
     printf("Test Typos:\n");
@@ -96,11 +96,27 @@ fn void build_test(char *cmd)
     build_cmd_append(cmd, "valgrind ");
     build_cmd_append(cmd, " --leak-check=full --track-origins=yes ");
     build_cmd_append(cmd, "./"BUILD_DIR"/"PROJECT_NAME);
-    // build_cid_append(cmd, " --leak-check=full --show-leak-kinds=all");
+    build_cmd_append(cmd, " --leak-check=full --show-leak-kinds=all");
     build_cmd_finish(cmd);
 }
 
-int main(int argc, char *argv[])
+internal void build_profiler(char *cmd)
+{
+    printf("Compiling:\n");
+    build_compile_cc(cmd);
+    build_cmd_append(cmd, " -Wno-unused-variable -Wno-unused-parameter -Wno-unused-function -Wno-missing-braces");
+    build_cmd_finish(cmd);
+
+    printf("Profiler Recording:\n");
+    build_cmd_append(cmd, "perf record -o ./"BUILD_DIR"/perf.data -g ./"BUILD_DIR"/"PROJECT_NAME);
+    build_cmd_finish(cmd);
+
+    printf("Profiler Report:\n");
+    build_cmd_append(cmd, "perf report -i ./"BUILD_DIR"/perf.data");
+    build_cmd_finish(cmd);
+}
+
+internal void entry_point(char *argv[])
 {
     // char *cmd = getenv("CMDLINE");
     char *option = argv[1];
@@ -113,10 +129,14 @@ int main(int argc, char *argv[])
     } else if (str8_match(str8_from_cstr(option), str8("build-run"))) {
         build_compile(cmd);
         build_run(cmd);
+    } else if (str8_match(str8_from_cstr(option), str8("build-debugger"))) {
+        build_debugger(cmd);
     } else if (str8_match(str8_from_cstr(option), str8("run"))) {
         build_run(cmd);
     } else if (str8_match(str8_from_cstr(option), str8("test"))) {
         build_test(cmd);
+    } else if (str8_match(str8_from_cstr(option), str8("profiler"))) {
+        build_profiler(cmd);
     } else if (
         str8_match(str8_from_cstr(option), str8("version")) ||
         str8_match(str8_from_cstr(option), str8("--version")) ||
@@ -135,18 +155,12 @@ int main(int argc, char *argv[])
     }
 }
 
-
-fn bool build_is_cpp_file(Str8 filename)
-{
-    return str8_ends_with(filename, str8(".cpp"));
-}
-
-fn void build_cmd_append(char *cmd, char *src)
+internal void build_cmd_append(char *cmd, char *src)
 {
 	strcat(cmd, src);
 }
 
-fn void build_cmd_run(char *cmd)
+internal void build_cmd_run(char *cmd)
 {
 	printf("%s\n\n", cmd);
 	int err = system(cmd);
@@ -157,7 +171,7 @@ fn void build_cmd_run(char *cmd)
 	}
 }
 
-fn void build_cmd_finish(char *cmd)
+internal void build_cmd_finish(char *cmd)
 {
 	build_cmd_run(cmd);
 	mem_set(cmd, 0, cstr8_length(cmd));
